46e730a34fec8a5b15020287c0b62f13aff87583
altaf <mobile.altaf@spaculus.info>
2025-04-11 19:16:53 +0530

    fix: replace missing drawable with built-in Android icon

--- packages/activity_recognition_flutter/android/src/main/java/dk/cachet/activity_recognition_flutter/ActivityRecognitionFlutterPlugin.java
+++ packages/activity_recognition_flutter/android/src/main/java/dk/cachet/activity_recognition_flutter/ActivityRecognitionFlutterPlugin.java
@@ -108,7 +108,7 @@ public class ActivityRecognitionFlutterPlugin implements FlutterPlugin, EventCha
         // Pass the notification title/text/icon to the service
         intent.putExtra("title", "MonsensoMonitor")
                 .putExtra("text", "Monsenso Foreground Service")
-                .putExtra("icon", R.drawable.common_full_open_on_phone)
+            .putExtra("icon", android.R.drawable.ic_menu_mylocation)
                 .putExtra("importance", 3)
                 .putExtra("id", 10);
 
--- packages/activity_recognition_flutter/android/src/main/java/dk/cachet/activity_recognition_flutter/ForegroundService.java
+++ packages/activity_recognition_flutter/android/src/main/java/dk/cachet/activity_recognition_flutter/ForegroundService.java
@@ -66,7 +66,8 @@ public class ForegroundService extends Service {
 
         // Get notification icon
 //        int icon = getResources().getIdentifier((String) extras.get("icon"), "drawable", context.getPackageName());
-        int icon = R.drawable.common_full_open_on_phone;
+       int icon = android.R.drawable.ic_menu_mylocation; // ✅ built-in icon
+
         
         // Make notification
         Notification notification = new Notification.Builder(context, "foreground.service.channel")
